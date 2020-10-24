17b2253e73156066183839cfc4afe06da388c922
jian-kowalski <jian_kowalski@hotmail.com>
2020-10-24 20:16:30 -0300

    Inclusao de pedido e item pedido

---
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,77 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pedido {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  private Long id;
+
+  @Column(name = "valor_total", nullable = false)
+  private BigDecimal valorTotal;
+
+  @Column(name = "taxa_frete", nullable = false)
+  private BigDecimal taxaFrete;
+
+  @Column(nullable = false)
+  private BigDecimal subTotal;
+
+  @CreationTimestamp
+  @Column(name = "data_criacao", nullable = false, columnDefinition = "dateTime")
+  private LocalDateTime dataCriacao;
+
+  @Column(name = "data_confirmacao", columnDefinition = "dateTime")
+  private LocalDateTime dataConfirmacao;
+
+  @Column(name = "data_cancelamento",columnDefinition = "dateTime")
+  private LocalDateTime dataCancelamento;
+
+  @Column(name = "data_entrega",columnDefinition = "dateTime")
+  private LocalDateTime dataEntrega;
+
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  private FormaPagamento formaPagamento;
+
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  private Restaurante restaurante;
+
+  @ManyToOne
+  @JoinColumn(name = "usuario_cliente_id", nullable = false)
+  private Usuario usuario;
+
+  @Column(nullable = false)
+  private StatusPedido status;
+
+  @Embedded
+  private Endereco enderecoEntrega;
+
+  @OneToMany(mappedBy = "pedido")
+  private List<ItemPedido> itens = new ArrayList<>();
+
+
+}
