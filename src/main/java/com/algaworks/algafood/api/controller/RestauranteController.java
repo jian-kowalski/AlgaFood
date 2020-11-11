2ee6208f007ff6d02f863ef88064270a3a1ed133
jian-kowalski <jian_kowalski@hotmail.com>
2020-11-11 14:13:51 -0300

    Finalizando tratamento de excecao da api

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,16 +6,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -104,21 +110,31 @@ public class RestauranteController {
     }
 
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
+            HttpServletRequest request) {
         Restaurante restauranteAtual = cadastroRestaurante.buscar(restauranteId);
-        marge(campos, restauranteAtual);
+        marge(campos, restauranteAtual, request);
         return alterar(restauranteId, restauranteAtual);
     }
 
-    private void marge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-            field.setAccessible(true);
-            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-            ReflectionUtils.setField(field, restauranteDestino, novoValor);
-        });
+    private void marge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+        try {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+                field.setAccessible(true);
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+                ReflectionUtils.setField(field, restauranteDestino, novoValor);
+            });
+        } catch (IllegalArgumentException ex) {
+            throw new HttpMessageNotReadableException(ex.getMessage(), ExceptionUtils.getRootCause(ex), new ServletServerHttpRequest(request));
+        }
     }
 
 }
--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-  ENTIDADE_NAO_ENCONTRADA("entidade-nao-encontrada", "Entidade nao encontrada"), 
   ERRO_NEGOCIO("erro-negocio", "Violação de regra de negócio"),
   ENTIDADE_EM_USO("entidade-em-uso", "Entidade em uso"),
-  MENSAGEM_INCOMPREENSIVEL("mensagem-incompreensivel","Mensagem nao interpretada corretamente");
+  MENSAGEM_INCOMPREENSIVEL("mensagem-incompreensivel","Mensagem nao interpretada corretamente"),
+  PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+  RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+  ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
 
   private String title;
   private String uri;
