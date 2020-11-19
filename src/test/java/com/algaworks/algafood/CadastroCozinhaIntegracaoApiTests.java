5731eeb805e8b9fb9d620bcc52732525f856a57c
jian-kowalski <jian_kowalski@hotmail.com>
2020-11-19 16:41:16 -0300

    inclusão de testes de integração

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegracaoApiTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegracaoApiTests.java
@@ -1,9 +1,26 @@
 package com.algaworks.algafood;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 
 @SpringBootTest
 public class CadastroCozinhaIntegracaoApiTests {
-  // RestAssured.Given()
+    @LocalServerPort
+    private int port;
+
+    public void testarStatus200ConsultarCozinha() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
 }
