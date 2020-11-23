85d613429e8e07db3785c58a0faf5ea23453cd81
jian-kowalski <jian_kowalski@hotmail.com>
2020-11-23 17:14:21 -0300

    Criação de classes mixin

--- src/main/java/com/algaworks/algafood/core/validation/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/validation/jackson/JacksonMixinModule.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.core.validation.jackson;
 
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.mixin.CidadeMixin;
 import com.algaworks.algafood.domain.model.mixin.RestauranteMixin;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -16,6 +19,8 @@ public class JacksonMixinModule extends SimpleModule{
   
   public JacksonMixinModule(){
     setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+    setMixInAnnotation(Cidade.class, CidadeMixin.class);
+    setMixInAnnotation(Cozinha.class, Cozinha.class);
   }
 
 }
